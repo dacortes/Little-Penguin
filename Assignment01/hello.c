1dda03006f6797d165a9fcf19ee14ed28342e2cd
dacortes <87203430+dacortes@users.noreply.github.com>
2025-11-02 15:10:41 +0000

    Refactor code structure for improved readability and maintainability

--- Assignment01/hello.c
+++ Assignment01/hello.c
@@ -2,6 +2,9 @@
 #include <linux/module.h>
 #include <linux/printk.h>
 
+#define AUTHOR "dacortes"
+#define DESCRIPTION "Simple Hello World Module for kernel 6.16.1"
+#define VERSION "0.1"
 static int __init hello_init(void)
 {
 	pr_info("Hello World\n");
@@ -18,3 +21,6 @@ module_init(hello_init);
 module_exit(hello_exit);
 
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR(AUTHOR);
+MODULE_DESCRIPTION(DESCRIPTION);
+MODULE_VERSION(VERSION);
