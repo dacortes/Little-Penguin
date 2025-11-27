7edd329dbc6671720b939225cf8fac23171a9574
root <root@dacortes.localdomain>
2025-11-27 15:42:37 +0100

    [RM] files

--- Assignment09/.mymounts.ko.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_mymounts.ko := ld -r -m elf_x86_64 -z noexecstack --no-warn-rwx-segments --build-id=sha1  -T /sources/linux-next-next-20251114/scripts/module.lds -o mymounts.ko mymounts.o mymounts.mod.o .module-common.o
